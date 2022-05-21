0cf2f0dd5d412e01e87c17bb4fc1d3b7dc764448
Henju-Code <henjucode@gmail.com>
2022-05-21 12:54:16 -0300

    fist commit

---
+++ src/modules/cars/useCases/importCategories/index.ts
@@ -0,0 +1,13 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoryRepository";
+import { ImportCategoriesController } from "./ImportCategoriesController";
+import { ImportCategoriesUseCase } from "./ImportCategoriesUseCase";
+
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoriesUseCase = new ImportCategoriesUseCase(
+    categoriesRepository
+);
+const importCategoryController = new ImportCategoriesController(
+    importCategoriesUseCase
+);
+
+export { importCategoryController };
